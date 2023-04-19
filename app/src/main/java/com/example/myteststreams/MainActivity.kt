0bca75494bd93533ba6b109024136215d270997f
alexanderrodin <pass.9157@gmail.com>
2023-04-19 11:32:57 +0300

    Initial commit

--- app/src/main/java/com/example/myteststreams/MainActivity.kt
+++ app/src/main/java/com/example/myteststreams/MainActivity.kt
@@ -19,21 +19,16 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-// TODO
-//    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-//        TODO Coroutines
         binding.buttonLoad.setOnClickListener {
-            lifecycleScope.launch{
+            lifecycleScope.launch {
                 loadData()
             }
         }
 
-//        TODO Использование Thread, потоков
-//        binding.buttonLoad.setOnClickListener { loadData() }
     }
 
     private suspend fun loadData() {
@@ -62,45 +57,4 @@ class MainActivity : AppCompatActivity() {
         delay(5000)
         return "Moscow"
     }
-
-
-//TODO Использование Thread, потоков
-//    private fun loadData() {
-//        Log.d("MainActivity", "Load started: $this")
-//        binding.progress.isVisible = true
-//        binding.buttonLoad.isEnabled = false
-//        loadCity {
-//            binding.tvLocation.text = it
-//            loadTemperature(it) {
-//                binding.tvTemperature.text = it.toString()
-//                binding.progress.isVisible = false
-//                binding.buttonLoad.isEnabled = true
-//                Log.d("MainActivity", "Load started: $this")
-//            }
-//        }
-//    }
-// TODO
-//    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
-//        thread {
-//            runOnUiThread {
-//                Toast.makeText(
-//                    this,
-//                    getString(R.string.loading_temperature_toast, city),
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//            }
-//            Thread.sleep(5000)
-//            runOnUiThread {
-//                callback.invoke(17)
-//            }
-//        }
-//    }
-//    private fun loadCity(callback: (String) -> Unit) {
-//        thread {
-//            Thread.sleep(5000)
-//            runOnUiThread {
-//                callback.invoke("Moscow")
-//            }
-//        }
-//    }
 }
