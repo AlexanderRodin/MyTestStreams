a83a6a58f07efafa94324cd1f4a68fe584438832
alexanderrodin <pass.9157@gmail.com>
2023-04-20 10:24:33 +0300

    Exception Handling

--- app/src/main/java/com/example/myteststreams/MainActivity.kt
+++ app/src/main/java/com/example/myteststreams/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.myteststreams.databinding.ActivityMainBinding
 import kotlinx.coroutines.Deferred
@@ -17,48 +18,13 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.buttonLoad.setOnClickListener {
-            binding.progress.isVisible = true
-            binding.buttonLoad.isEnabled = false
-            val deferredCity: Deferred<String> = lifecycleScope.async {
-                val city = loadCity()
-                binding.tvLocation.text = city
-                city
-            }
-            val deferredTemp: Deferred<Int> = lifecycleScope.async {
-                val temp = loadTemperature()
-                binding.tvTemperature.text = temp.toString()
-                temp
-            }
-            lifecycleScope.launch {
-
-                val city = deferredCity.await()
-                val temp = deferredTemp.await()
-                Toast.makeText(this@MainActivity, "City: $city Temp: $temp", Toast.LENGTH_SHORT)
-                    .show()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-
-            }
-        }
-
-    }
-
-    private suspend fun loadData() {
-
-
-    }
-
-    private suspend fun loadTemperature(): Int {
-        delay(5000)
-        return 17
-    }
-
-    private suspend fun loadCity(): String {
-        delay(5000)
-        return "Moscow"
+        viewModel.method()
     }
 }
